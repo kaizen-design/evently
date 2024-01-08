97dc4db0b0b5464669eca18ae302e064eed7186b
kaizen-design <denis@kaizen-design.ru>
2024-01-08 22:44:05 +0300

    Assets added

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <Button variant="outline" className='mt-3 px-4'>Hello</Button>
-  )
-}
