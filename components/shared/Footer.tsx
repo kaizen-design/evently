75170ce7bea8297d45f9a6bba56de9ebf01447c6
kaizen-design <denis@kaizen-design.ru>
2024-01-09 18:02:41 +0300

    Footer completed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="wrapper flex-center flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image 
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently"
+          />
+        </Link>
+        <p>&copy; {new Date().getFullYear()} Evently. All Rights Reserved.</p>
+      </div>
+    </footer>
   )
 };
 
