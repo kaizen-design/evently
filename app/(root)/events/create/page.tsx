0938c6d54140826a3061bbdd6ad6a6de15a4376c
kaizen-design <denis@kaizen-design.ru>
2024-01-11 19:49:55 +0300

    Create event page created

---
+++ app/(root)/events/create/page.tsx
@@ -0,0 +1,18 @@
+import EventForm from "@/components/shared/EventForm";
+import { auth } from "@clerk/nextjs";
+
+export default function CreateEvent() {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h1 className="wrapper h3-bold text-center sm:text-left">Create Event</h1>
+      </section>
+      <div className="wrapper my-10">
+        <EventForm userId={userId} type="Create" />
+      </div>
+    </>  
+  )
+}
