92821fc7f86cc4862fefab62fbca8b6c0e88b35f
kaizen-design <denis@kaizen-design.ru>
2024-01-16 18:30:47 +0300

    Profile page created

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,69 @@
+import Collection from "@/components/shared/Collection";
+import { Button } from "@/components/ui/button";
+import { getEventsByUser } from "@/lib/actions/event.actions";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default async function Profile() {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const myEvents = await getEventsByUser({
+    userId: userId,
+    limit: 6,
+    page: 1
+  });
+
+  console.log(myEvents)
+
+  return (
+    <>
+      {/* MY TICKETS */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h1 className="h3-bold text-center sm:text-left">My Tickets</h1>
+          <Button asChild className="button hidden sm:flex" size="lg">
+            <Link href="/#events">
+              Explore More Events
+            </Link>
+          </Button>
+        </div>
+      </section>
+      {/* <section className="wrapper my-8">
+        <Collection 
+          data={events?.data}
+          emptyTitle="No event tickets purchased yet."
+          emptyStateSubtext="No worries – plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={1}
+          urlParamName="ordersPage"
+          totalPages={events?.totalPages}
+        />
+      </section> */}
+      {/* MY EVENTS */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h1 className="h3-bold text-center sm:text-left">Events Organized</h1>
+          <Button asChild className="button hidden sm:flex" size="lg">
+            <Link href="/events/create">
+              Create New Event
+            </Link>
+          </Button>
+        </div>
+      </section>
+      <section className="wrapper my-8">
+        <Collection 
+          data={myEvents?.data}
+          emptyTitle="No events have been created yet."
+          emptyStateSubtext="Go create some now!"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          urlParamName="eventsPage"
+          totalPages={myEvents?.totalPages}
+        />
+      </section>
+    </>
+  )
+}
--- components/shared/EventCard.tsx
+++ components/shared/EventCard.tsx
@@ -57,10 +57,7 @@ const EventCard = ({
         </div>
       )}
       {/* IS EVENT CREATOR... */} 
-      <Link 
-        href={`/events/${event._id}`}
-        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
-      >
+      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2 ">
             <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
@@ -74,7 +71,11 @@ const EventCard = ({
         <p className="p-medium-16 md:p-medium-18 text-grey-500">
           {formatDateTime(event.startDateTime).dateTime}
         </p>
-        <h3 className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{event.title}</h3>
+        <h3 className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          <Link href={`/events/${event._id}`}>
+            {event.title}
+          </Link>
+        </h3>
         <div className="flex-between w-full ">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
             {event.organizer.firstName} {event.organizer.lastName}
@@ -91,7 +92,7 @@ const EventCard = ({
             </Link>
           )}
         </div>
-      </Link>
+      </div>
     </div>
   )
 };
